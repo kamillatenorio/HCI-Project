a990be0d0bb6bb5fcbe5298d046f58f86d8b22a6
Kamilla Tenório <kktas@ic.ufal.br>
2021-07-19 18:23:30 -0300

    Adding Navbar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import  '../App.css';
+
+function NavBar() {
+    return (
+        <div className="NavBar"> 
+            <div className="logo">EXPERIMENTE</div>
+        </div>
+    )
+}
+
+export default NavBar;
