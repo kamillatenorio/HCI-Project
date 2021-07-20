3774fa7857871ac529889f0eaaf8649fdb7f8188
Kamilla Tenório <kktas@ic.ufal.br>
2021-07-19 23:07:56 -0300

    Updating Login Component

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends Component {
-
   render() {
     return (
-        <div></div>
+        <div >
+                <p >Sign in for EXPERIMENTE</p>
+                <form >
+                    <label for="username">Username</label>
+                    <input type="text" class="input_data" id="username"/>
+                    
+                    <label for="password">Password</label>
+                    <input type="text" class="input_data" id="password"/>
+
+                    <Link to={{ pathname: '/userprofile' }}  >
+                        <button onClick={this.selladd}>Sign in</button>
+                    </Link>
+
+                    <a href="#" >Forgot your password?</a>
+                </form>
+            </div>
     )
   }
 }
