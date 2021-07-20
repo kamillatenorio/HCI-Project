758f7912bd80fb456c27ac8b3753b448b261acf2
Kamilla Tenório <kktas@ic.ufal.br>
2021-07-20 09:08:05 -0300

    Adding Components of the Experiment Phases

---
+++ src/views/experimentPhases/Planning.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import  '../App.css';
+
+class Planning extends Component {
+  render() {
+    return (
+      <div className="content">
+
+    </div>
+    )
+  }
+}
+
+export default Planning;
---
+++ src/views/experimentPhases/Report.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import  '../App.css';
+
+class Report extends Component {
+  render() {
+    return (
+      <div className="content">
+
+    </div>
+    )
+  }
+}
+
+export default Report;
