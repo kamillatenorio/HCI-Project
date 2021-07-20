5ce08b8aa53042c9a72dbcd740eea637a393faa5
Kamilla Tenório <kktas@ic.ufal.br>
2021-07-20 07:56:53 -0300

    Correcting navegation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import  '../App.css';
 import BRFlag from '../assets/img/brazil.png';
 import USAFlag from '../assets/img/usa.png';
+import { Router } from 'react-router-dom';
+
 
 function NavBar() {
+    const currentUrl = window.location.href;
+    // const currentUrl = useLocation();
     return (
         <div className="NavBar"> 
             <div className="leftSide">
@@ -14,15 +18,20 @@ function NavBar() {
             <div className="rightSide">
                 <div>
                     <div className="links">
-                        <a href="/login"><img src= {BRFlag} alt="BRflag" className="BRflag"/> </a> 
-                        <a href="/login"><img src= {USAFlag} alt="BRflag" className="USAflag"/>  </a>   
+                        <img src= {BRFlag} alt="BRflag" className="BRflag"/> &nbsp;
+                        <img src= {USAFlag} alt="BRflag" className="USAflag"/>  &nbsp;
                     </div>
                 </div>
                 <div>
+                {currentUrl === "http://localhost:3000/" || currentUrl === "http://localhost:3000/login" || currentUrl === "http://localhost:3000/signup" ? (
                     <div className="linkSign">
-                        <a href="/login">SIGN IN  |  </a>     
-                        <a href="/signup">SIGN UP</a>
+                        <a href="/login"> <b>SIGN IN </b> |  </a>     
+                        <a href="/signup"> <b>SIGN UP </b> </a>
+                    </div>):(
+                    <div className="linkSign">
+                        <a href="/login"> <b>LOG OUT </b></a>
                     </div>
+                    )}
                 </div>
             </div>
         </div>
