8c0ec2467dd8baf9b00e39a5d243aa43d8f8dd0b
Kamilla Tenório <kktas@ic.ufal.br>
2021-07-20 07:58:56 -0300

    Adding new path: NewExperiment

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { BrowserRouter, Switch, Route,  Redirect } from 'react-router-dom';
 import Login from '../src/views/Login.jsx';
 import SignUp from '../src/views/SignUp.jsx';
 import UserProfile from '../src/views/UserProfile.jsx';
+import NewExperiment from './views/NewExperiment';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -15,6 +18,7 @@ function App() {
             <Route exact path="/" component = {SignUp}/>
             <Route path="/login" component = {Login}/>
             <Route path="/userprofile" component = {UserProfile}/>
+            <Route path="/newexperiment" component = {NewExperiment}/>
             <Redirect to="/"/>
         </Switch>
       </ BrowserRouter>    
