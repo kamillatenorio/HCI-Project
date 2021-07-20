f24a23a28e6ac204aaebbc550299b4024b9dbd79
Kamilla Tenório <kktas@ic.ufal.br>
2021-07-19 23:08:53 -0300

    Updating App.js component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import NavBar from './components/NavBar';
-import Login from '../src/views/Login';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route,  Redirect } from 'react-router-dom';
+import Login from '../src/views/Login.jsx';
+import SignUp from '../src/views/SignUp.jsx';
+import UserProfile from '../src/views/UserProfile.jsx';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <Login/>
+      <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component = {SignUp}/>
+            <Route path="/login" component = {Login}/>
+            <Route path="/userprofile" component = {UserProfile}/>
+            <Redirect to="/"/>
+        </Switch>
+      </ BrowserRouter>    
     </div>
   );
 }
