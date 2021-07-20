6eff6304019c79a4d9d77083d54433b18c138885
Kamilla Tenório <kktas@ic.ufal.br>
2021-07-20 07:55:57 -0300

    Adding Component of NewExperiment

---
+++ src/views/NewExperiment.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import  '../App.css';
+
+class NewExperiment extends Component {
+  render() {
+    return (
+      <div className="content">
+      </div>
+    )
+  }
+}
+
+export default NewExperiment;
