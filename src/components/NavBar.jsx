d4e882235d0758db54368295af7a398b42997042
Kamilla Tenório <kktas@ic.ufal.br>
2021-07-19 22:04:08 -0300

    Updating NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import  '../App.css';
+import BRFlag from '../assets/img/brazil.png';
+import USAFlag from '../assets/img/usa.png';
 
 function NavBar() {
     return (
         <div className="NavBar"> 
-            <div className="logo">EXPERIMENTE</div>
+            <div className="leftSide">
+                <div className="links">
+                    <a href="/login">EXPERIMENT</a>
+                </div>
+            </div>
+            <div className="rightSide">
+                <div>
+                    <div className="links">
+                        <a href="/login"><img src= {BRFlag} alt="BRflag" className="BRflag"/> </a> 
+                        <a href="/login"><img src= {USAFlag} alt="BRflag" className="USAflag"/>  </a>   
+                    </div>
+                </div>
+                <div>
+                    <div className="linkSign">
+                        <a href="/login">SIGN IN  |  </a>     
+                        <a href="/login">SIGN UP</a>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
