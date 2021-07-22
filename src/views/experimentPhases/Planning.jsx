444d4cb806d4c7ed0bc0342631ad6fbc0289e03e
Kamilla Tenório <kktas@ic.ufal.br>
2021-07-21 21:10:43 -0300

    Adding planning code in the "Planning" component

--- src/views/experimentPhases/Planning.jsx
+++ src/views/experimentPhases/Planning.jsx
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import  '../../App.css';
+import NavBarExp from '../../components/NavBarExp';
+import { Tabs, Tab } from 'react-bootstrap';
 
 class Planning extends Component {
   render() {
     return (
-      <div className="content">
+      <div>
+        <NavBarExp/>
+        <div class="container" style={{ maxWidth: '65%' }}>
+          <Tabs id="uncontrolled-tab-example2" className="mb-3">
+            <Tab eventKey="planning" title="Planning">
+                  <div className="experimentDiv">
 
-    </div>
+                    <p className="experimentTitle">Context Detailing</p>
+                    <hr class="dotted experimentDot"></hr>
+                    <textarea name="quality" id="quality" className="experimentTextArea" rows='6' />
+
+                    <br />
+
+                    <button className="blue_button saveButton">Save</button>
+                  </div>
+            </Tab>
+          </Tabs>
+        </div>
+      </div>
     )
   }
 }
