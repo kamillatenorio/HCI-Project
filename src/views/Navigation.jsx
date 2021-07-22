dd9c07e73454e00394482769d8cbf1caf8ee5be2
Kamilla Tenório <kktas@ic.ufal.br>
2021-07-21 22:07:01 -0300

    Adding link to user profile

--- src/views/Navigation.jsx
+++ src/views/Navigation.jsx
@@ -4,8 +4,8 @@ class Navegation extends Component {
   render() {
     return (
         <div class="container" style={{ maxWidth: '65%' }}>
-            <h5 style={{color: '#727070', fontWeight: 'bold'}}> Joana Vicente | 
-                <span style={{color: '#727070'}}> Gamification and Flow Experience</span>
+            <h5 style={{color: '#727070', fontWeight: 'bold'}}> <a href="/userprofile">Joana Vicente</a>
+                <span style={{color: '#727070'}}> | Gamification and Flow Experience</span>
             </h5>
         </div>
     )
