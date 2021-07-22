ea9016773537b3d55909f543e65ea70946859125
Kamilla Tenório <kktas@ic.ufal.br>
2021-07-21 21:13:45 -0300

    Adding conduction code in the "Conduction" component

--- src/views/experimentPhases/Operation.jsx
+++ src/views/experimentPhases/Operation.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import  '../../App.css';
+import NavBarExp from '../../components/NavBarExp';
+import { Tabs, Tab } from 'react-bootstrap';
 
 class Operation extends Component {
   render() {
     return (
-      <div className="content">
-
-    </div>
+      <div>
+        <NavBarExp/>
+        <div class="container" style={{ maxWidth: '65%' }}>
+          <Tabs id="uncontrolled-tab-example2" className="mb-3">
+            <Tab eventKey="operation" title="Operation">
+              <p style={{ marginLeft: '14%' }}>The experiment operation will be made by another tool, which can be accessed <a href="#" target="__Blank">HERE</a>.</p>
+            </Tab>
+          </Tabs>
+        </div>
+      </div>
     )
   }
 }
