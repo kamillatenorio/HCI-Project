6eb3f28aeb1c3328c329ce6b6cd8c4c2ed468641
Kamilla Tenório <kktas@ic.ufal.br>
2021-07-21 21:40:21 -0300

    Adding "Navigation" component

---
+++ src/views/Navigation.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Navegation extends Component {
+  render() {
+    return (
+        <div class="container" style={{ maxWidth: '65%' }}>
+            <h5 style={{color: '#727070', fontWeight: 'bold'}}> Joana Vicente | 
+                <span style={{color: '#727070'}}> Gamification and Flow Experience</span>
+            </h5>
+        </div>
+    )
+  }
+}
+
+export default Navegation;
