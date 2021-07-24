83a55928786328ecbe3696edd60ae60a82fd8dde
Kamilla Tenório <kktas@ic.ufal.br>
2021-07-24 18:10:38 -0300

    Adding placeholders in the forms inputs of the Scope component

--- src/views/experimentPhases/Scope.jsx
+++ src/views/experimentPhases/Scope.jsx
@@ -19,34 +19,34 @@ class Scope extends Component {
                 <hr class="dotted experimentDot"></hr>
 
                 <label htmlFor="analyze" className="experimentLabel">Analyze</label>
-                <input className="experimentInput" type="text" name="analyze" id="analyze" placeholder='<Objext(s) of study>' />
+                <input className="experimentInput" type="text" name="analyze" id="analyze" placeholder='<Add the objext(s) of study>' />
 
                 <br />
                 <br />
 
                 <label htmlFor="purpose" className="experimentLabel">For the propose of</label>
-                <input type="text" name="purpose" id="purpose" placeholder='<Purpose>' className="experimentInput" />
+                <input type="text" name="purpose" id="purpose" placeholder='<Add the Purpose>' className="experimentInput" />
 
 
                 <br />
                 <br />
 
                 <label htmlFor="quality" className="experimentLabel">with respect to their </label>
-                <input type="text" name="quality" id="quality" placeholder='<Quality focus>' className="experimentInput" />
+                <input type="text" name="quality" id="quality" placeholder='<Add the quality focus>' className="experimentInput" />
 
 
                 <br />
                 <br />
 
                 <label htmlFor="perspective" className="experimentLabel">from the point of view of the</label>
-                <input type="text" name="perspective" id="perspective" placeholder=' <Perspective>' className="experimentInput" />
+                <input type="text" name="perspective" id="perspective" placeholder=' <Add the perspective>' className="experimentInput" />
 
                 <br />
                 <br />
 
 
                 <label htmlFor="context" className="experimentLabel">in the context of</label>
-                <input type="text" name="context" id="context" placeholder=' <Context>' className="experimentInput" />
+                <input type="text" name="context" id="context" placeholder=' <Add the context>' className="experimentInput" />
 
                 <button className='blue_button saveButton'>Save</button>
 
@@ -61,31 +61,31 @@ class Scope extends Component {
 
 
                 <label htmlFor="quality" className="experimentLabel">Population</label>
-                <input type="text" name="quality" id="quality" className="experimentInput" />
+                <input type="text" name="quality" id="quality" placeholder='Add the population in which the evidence is going to be collected.' className="experimentInput" />
 
                 <br />
                 <br />
 
                 <label htmlFor="quality" className="experimentLabel">Intervation</label>
-                <input type="text" name="quality" id="quality" className="experimentInput" />
+                <input type="text" name="quality" id="quality" placeholder='Add the intervention that is going to be applied in the empirical study.' className="experimentInput" />
 
                 <br />
                 <br />
 
                 <label htmlFor="quality" className="experimentLabel">Comparsion</label>
-                <input type="text" name="quality" id="quality" className="experimentInput" />
+                <input type="text" name="quality" id="quality" placeholder='Add the comparison to which the intervention is going to be compared.' className="experimentInput" />
 
                 <br />
                 <br />
 
                 <label htmlFor="quality" className="experimentLabel">Outcome</label>
-                <input type="text" name="quality" id="quality" className="experimentInput" />
+                <input type="text" name="quality" id="quality" placeholder='Add the expected outcome of the experiment.' className="experimentInput" />
 
                 <br />
                 <br />
 
                 <label htmlFor="quality" className="experimentLabel">Context</label>
-                <input type="text" name="quality" id="quality" className="experimentInput" />
+                <input type="text" name="quality" id="quality" placeholder='Add the context of the experiment.' className="experimentInput" />
 
                 <br />
 
@@ -101,13 +101,13 @@ class Scope extends Component {
 
 
                 <label htmlFor="quality" className="experimentLabel">RQ1</label>
-                <input type="text" name="quality" id="quality" className="experimentInput" value="Some question?" />
+                <input type="text" name="quality" id="quality" className="experimentInput" placeholder="Enter your first research question"/>
 
                 <br />
                 <br />
 
                 <label htmlFor="quality" className="experimentLabel">RQ2</label>
-                <input type="text" name="quality" id="quality" className="experimentInput" value="Some question 2?" />
+                <input type="text" name="quality" id="quality" className="experimentInput" placeholder="Enter your second research question" />
 
                 <br />
 
@@ -122,19 +122,19 @@ class Scope extends Component {
 
 
                 <label htmlFor="quality" className="experimentLabel"> What is already known about this topic?</label> <br />
-                <textarea name="quality" id="quality" className="experimentTextArea" rows='6' />
+                <textarea name="quality" id="quality" placeholder='Add what is already known about this subject according to the scientific literature.' className="experimentTextArea" rows='6' />
 
                 <br />
                 <br />
 
-                <label htmlFor="quality" className="experimentLabel"> What this paper add?</label> <br />
-                <textarea name="quality" id="quality" className="experimentTextArea" rows='6' />
+                <label htmlFor="quality" className="experimentLabel"> What this experiment add?</label> <br />
+                <textarea name="quality" id="quality" placeholder='Add what this experiment add in the current scientific literature.' className="experimentTextArea" rows='6' />
 
                 <br />
                 <br />
 
                 <label htmlFor="quality" className="experimentLabel"> Implications for practice and/or policy </label> <br />
-                <textarea name="quality" id="quality" className="experimentTextArea" rows='6' />
+                <textarea name="quality" id="quality" placeholder='Add the implications for practice and/or policy of this experiment.' className="experimentTextArea" rows='6' />
 
                 <br />
 
